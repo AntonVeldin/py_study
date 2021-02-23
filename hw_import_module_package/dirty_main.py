84789fd6d80a2995ba29aa988f18b5ad29caf06b
Anton Veldin <anton.veldin@gmail.com>
2021-02-23 22:54:39 +1000

    hw import_module_package; .py from orm lesson

---
+++ hw_import_module_package/dirty_main.py
@@ -0,0 +1,6 @@
+# from application import *
+from application.salary import *
+
+# salary.calculate_salary()
+calculate_salary()
+# Все не получается импортировать :(
--- sql_py_orm/py_sql_0_orm.py
+++ sql_py_orm/py_sql_0_orm.py
@@ -1,3 +1,5 @@
+import datetime
+
 import sqlalchemy as sq
 import psycopg2
 import json
@@ -47,6 +49,13 @@ class Album(Base):
     id_author = sq.Column(sq.Integer, sq.ForeignKey('author.id'))
     author = relationship(Author)
 
+    def init_publish(self, pub_date=None):
+        if pub_date is not None:
+            self.published = pub_date
+        else:
+            self.published = datetime.date.today()
+        session.commit()
+
 
 class Genre(Base):
     __tablename__ = 'genre'
@@ -75,40 +84,69 @@ class Track(Base):
 
 if __name__ == '__main__':
     # Создание всех таблиц:
-    Base.metadata.create_all(engine)
+    # Base.metadata.create_all(engine)
     session = Session()
-    art1 = Author(name='Best Bundle')
-    session.add(art1)
+#     art1 = Author(name='Best Bundle')
+#     session.add(art1)
+#     session.commit()
+#     date_arl1 = {
+#         'Album 1': [
+#             {'name': 'Track 1_1', 'dur': 60},
+#             {'name': 'Track 1_2', 'dur': 50},
+#             {'name': 'Track 1_3', 'dur': 45}
+#         ],
+#         'Album 2': [
+#             {'name': 'Track 2_1', 'dur': 60},
+#             {'name': 'Track 2_2', 'dur': 50},
+#             {'name': 'Track 2_3', 'dur': 45}
+#         ]
+#     }
+#
+#     for title, tracks in date_arl1.items():
+#         alb = Album(title=title, author=art1)
+#         session.add(alb)
+#         for tr in tracks:
+#             tr = Track(title=tr.get('name'), duration=tr.get('dur'), album=alb)
+#             # tr.id_album = alb (либо так)
+#             x = session.add(tr)
+#     session.commit()
+#
+# # в дебаггере  смотрим переменные,
+# # art1
+# # art1.albums
+# # art1.name
+# # art1.id
+# # alb.id
+# # tr.album
+# # tr.album.title
+
+# Корректировка записей в БД:
+    session.query(Album).filter(Album.title == 'Album 2').update({
+        'published': datetime.date(year=2020, month=1, day=1)
+    })
     session.commit()
-    date_arl1 = {
-        'Album 1': [
-            {'name': 'Track 1_1', 'dur': 60},
-            {'name': 'Track 1_2', 'dur': 50},
-            {'name': 'Track 1_3', 'dur': 45}
-        ],
-        'Album 2': [
-            {'name': 'Track 2_1', 'dur': 60},
-            {'name': 'Track 2_2', 'dur': 50},
-            {'name': 'Track 2_3', 'dur': 45}
-        ]
-    }
+x = path
 
-    for title, tracks in date_arl1.items():
-        alb = Album(title=title, author=art1)
-        session.add(alb)
-        for tr in tracks:
-            tr = Track(title=tr.get('name'), duration=tr.get('dur'), album=alb)
-            # tr.id_album = alb (либо так)
-            session.add(tr)
+# Заполнение промежуточной таблицы с жанрами:
+#     genres = ['rock', 'classic', 'jazz']
+#     inst_genres = []
+#     for g in genres:
+#         inst_genres.append(Genre(title=g))
+#         print(inst_genres)
+#     session.add_all(inst_genres)
+#     for track in session.query(Track).filter(Track.id_album == 2).all():
+#         track.genre.extend(inst_genres)
+#     session.commit()
+
+    q = session.query(Author).filter(Author.name == 'Best Bundle')
+    artist = q.first()
+    # artist.albums[1].track[0].genre[0].title  в дебаггере
+    # [g.title for g in artist.albums[1].track[0].genre] в дебаггере
+    # session.query(Genre).first().track в дебаггере
+
+    # alb1 = artist.albums[0]
+    # alb1.init_publish()
+    # alb1.published
+    # datetime.date(2021, 2, 23) в дебаггере
+
     session.commit()
-
-
-
-# в дебаггере  смотрим переменные,
-# art1
-# art1.albums
-# art1.name
-# art1.id
-# alb.id
-# tr.album
-# tr.album.title
